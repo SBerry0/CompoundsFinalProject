a780f10c6549d05c73cec02a7a3ff9ff76df5d77
Sohum Berry <sohumberry@gmail.com>
2025-05-09 00:23:34 -0700

    Eleventh Coding Session

--- clean.py
+++ clean.py
@@ -8,34 +8,34 @@ output_file = "data/cpdat_merged_files.csv"
 # Load both files
 list_presence_df = pd.read_csv(list_file, low_memory=False)
 print(list_presence_df.shape)
-list_presence_df = list_presence_df.dropna(subset=['dtxsid'])
-print(list_presence_df.shape)
-# functional_df = pd.read_csv(functional_file, low_memory=False)
+list_presence_df = list_presence_df.dropna(subset=['curated_casrn'])
+# print(list_presence_df.shape)
+functional_df = pd.read_csv(functional_file, low_memory=False)
+functional_df = functional_df.dropna(subset=['curated_casrn'])
 
 # Select relevant columns
-# functional_cols = functional_df[[
-#     'data_source', 'data_group', 'curated_chemical_name', 'curated_casrn', 'dtxsid',
-#     'raw_functional_use', 'function_category', 'component_name'
-# ]]
-
-list_cols = list_presence_df[[
-    'data_source', 'curated_chemical_name', 'curated_casrn', 'dtxsid',
-    'data_group', 'organization', 'component_name', 'keyword_set'
+functional_cols = functional_df[[
+    'data_source', 'data_group', 'curated_chemical_name', 'curated_casrn', 'dtxsid',
+    'raw_functional_use', 'function_category', 'component_name'
 ]]
 
-for col in list_cols:
-    print(list_presence_df.iloc[9][col])
+# list_cols = list_presence_df[[
+#     'data_source', 'curated_chemical_name', 'curated_casrn', 'dtxsid',
+#     'data_group', 'organization', 'component_name', 'keyword_set'
+# ]]
 
-list_presence_df = list_presence_df.dropna(subset=['component_name'])
-print(list_presence_df.shape)
+# for col in list_cols:
+#     print(list_presence_df.iloc[9][col])
+
+# list_presence_df = list_presence_df.dropna(subset=['component_name'])
+# print(list_presence_df.shape)
 
 
 
-for col in list_cols:
-    print(list_presence_df.iloc[9][col])
+# for col in list_cols:
+#     print(list_presence_df.iloc[9][col])
 
-print(list_presence_df.head(5))
-
+# print(list_presence_df.head(5))
 # list_clean = list_presence_df[['curated_chemical_name', 'dtxsid', 'data_']]
 
 print('--------------------------------------------------------')
@@ -51,55 +51,79 @@ for col in list_presence_df.columns:
 from collections import defaultdict
 
 
-ids = defaultdict(list)
+ids = {}
 
 for _, row in list_presence_df.iterrows():
-    ids[row['curated_casrn']] = [row['curated_chemical_name'], row["component_name"]]
+    ids[row['curated_casrn']] = row['curated_chemical_name']
 
 
-print(ids)
+# print(ids)
 print(len(ids))
 
-
 import cirpy
+from concurrent.futures import ThreadPoolExecutor
+import time
 
-for key, value in ids.items():
-    smiles = cirpy.resolve(key, 'smiles')
-    name = cirpy.resolve(key, 'names')
-    # print(name)
-    ids[key].append(smiles)
-    ids[key].append(name)
+def resolve_all(key):
+    time.sleep(0.5)
+    return {
+        'key': key,
+        'smiles': cirpy.resolve(key, 'smiles'),
+        'name': cirpy.resolve(key, 'names'),
+        'formula': cirpy.resolve(key, 'formula')
+    }
 
-print(ids)
+results = []
+i=0
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for result in executor.map(resolve_all, ids.keys()):
+        if i % 200 == 0:
+            print(i, "/", len(ids))
+        results.append(result)
+        i+=1
+
+
+# for key, value in ids.items():
+
+#     smiles = cirpy.resolve(key, 'smiles')
+#     name = cirpy.resolve(key, 'names')
+#     formula = cirpy.resolve(key, 'formula')
+#     # print(name)
+#     ids[key].append(smiles)
+#     ids[key].append(name)
+#     ids[key].append(formula)
+#     i+=1
+
+# print(ids)
 
 
 class Chemical:
-    def __init__(self, cas: str, smiles: str, chemical_name: str, component_name: str, names: [str]):
+    def __init__(self, cas: str, smiles: str, formula: str, chemical_name: str, names: [str]):
         self.casid = cas
         self.smiles = smiles
+        self.formula = formula
         self.chemical_name = chemical_name
-        self.component_name = component_name
         self.names = names
 
     def to_dict(self):
         return {'casid': self.casid,
                 'smiles': self.smiles,
+                'formula': self.formula,
                 'chemical_name': self.chemical_name,
-                'component_name': self.component_name,
                 'name': self.names}
 
     @classmethod
     def from_dict(cls, d):
-        return cls(d['casid'], d['smiles'], d['chemical_name'], d['component_name'], d['name'])
+        return cls(d['casid'], d['smiles'], d['formula'], d['chemical_name'], d['name'])
 
 chemicals = []
 
-for key, value in ids.items():
-    chemicals.append(Chemical(key, value[2], value[0], value[1], value[3]))
+for res in results:
+    chemicals.append(Chemical(res['key'], res['smiles'], res['formula'], ids[res['key']], res['name']))
 
 import json
 
-with open('chemicals.json', 'w') as f:
+with open('chemicals_large.json', 'w') as f:
     json.dump([c.to_dict() for c in chemicals], f, indent=2)
 
 # with open('chemicals.json', 'r') as f:
